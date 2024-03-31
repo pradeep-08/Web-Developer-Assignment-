ac56fe011609b6460aa2a014f1e1ca4c4b646a3d
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-31 22:35:25 +0530

    bar-2
    some decors on the bar 2

--- src/Components/asset/bar3.js
+++ src/Components/asset/bar3.js
@@ -6,7 +6,9 @@ import '../style/bar3.css';
 function Bar3() {
   return <>
    <div className="bar-3">
-        <div className="bar-3-contents-1"></div>
+        <div className="bar-3-contents-1">
+            
+        </div>
             <div className="bar-3-contents-2"></div>
         </div>
   </>
