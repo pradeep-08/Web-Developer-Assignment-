3389809adfd3974652b29b2fc606b11db3d48247
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-31 19:01:57 +0530

    mainboard
    constructing layout basement using more Div

--- src/Components/asset/mainboard.js
+++ src/Components/asset/mainboard.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../style/mainboard.css';
 
 function Mainboard() {
   return <>
-    <div className='main-board-div'>mainboard </div>
+    <div className='main-board-div'>
+        <div className="bar-1"></div>
+        <div className="bar-2">
+            <div className="bar-2-contents"></div>
+            <div className="bar-2-contents"></div>
+            <div className="bar-2-contents"></div>
+            <div className="bar-2-contents"></div>
+        </div>
+        <div className="bar-3">
+        <div className="bar-3-contents-1"></div>
+            <div className="bar-3-contents-2"></div>
+        </div>
+        <div className="bar-4"></div>
+        
+    </div>
   </>
 }
 
