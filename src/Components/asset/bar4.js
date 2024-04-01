adb1742f7119ecc0fb2cb77e413c6348237eb776
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-04-01 15:42:59 +0530

    Without response
    fully finished dashboard without Responsive

--- src/Components/asset/bar4.js
+++ src/Components/asset/bar4.js
@@ -5,10 +5,10 @@ import '../style/bar4.css';
 function Bar4() {
   return <>
         <div className="bar-4">
-        <div className="bar-4-contents-1">
-           <div className="bar-filled-contents">
-              <h3 style={{ color:"#1e327b"}}>Product Sell</h3>
-           <div className='bar-4-contains'>
+          <div className="bar-4-contents-1">
+            <div className="bar-filled-contents">
+                <h3 style={{ color:"#1e327b"}}>Product Sell</h3>
+              <div className='bar-4-contains'>
                <div className="username-1"> 
                    <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png" alt="search--v1"/>
                    <input type="text" placeholder='Search' size="15"/>
@@ -16,8 +16,150 @@ function Bar4() {
               <div className="drop-down">Last 30 days<img width="10" height="10" src="https://img.icons8.com/metro/26/EBEBEB/sort-down.png" alt="sort-down"/></div>
           </div>
        </div>
-        </div>
-        <div className="bar-4-contents-2"></div>
+       <div className="bar-4-contents-2">
+       <div className="bar-filled-contents">
+        <div style={{ color:"#C9C9D0", fontWeight:"500"}}>Product name</div>
+           <div className='bar-4-contains'>
+                <div className="bar-2-details"> 
+                <div style={{ color:"#C9C9D0", fontWeight:"500"}}>Stock</div>
+                <div style={{ color:"#C9C9D0", fontWeight:"500"}}>Price</div>
+                <div style={{ color:"#C9C9D0", fontWeight:"500"}}>Total Sales</div>
+              </div>
+          </div>
+       </div>
+                     <hr />
+                </div> 
+
+
+            <div className="product-list-main">
+                <div className="product-list-child-1">
+                  <div>
+                      <img style={{borderRadius:"5px"}} width="80" height="50" src="https://images.saasworthy.com/tr:w-160,h-0,c-at_max,e-sharpen-1/adobephotoshop_10786_logo_1632385034_7fozn.svg" alt="" />
+                      </div>
+                      <div className="product-details-info">
+                      <div className="product-name" >
+                            Adobe Photoshop Elements
+                        </div>
+                          <div className="product-details">
+                          Adobe Photoshop Elements is a latest raster graphic editor from the house of Adobe.
+                      </div>
+                      </div>
+                        
+                   </div>
+                <div style={{paddingRight: "18px;"}} className="product-list-child-2">
+                    <div className="bar-2-details"> 
+                      <div style={{  color:"#818181", fontWeight:"500"}}>08 in stock</div>
+                      <div style={{ color:"black" , fontWeight:"bold"}}>$ 45.99</div>
+                      <div style={{  color:"#818181", fontWeight:"500"}}>20</div>
+                   </div>
+                </div>        
+            </div>
+            <div className="product-list-main">
+                <div className="product-list-child-1">
+                  <div>
+                      <img style={{borderRadius:"5px"}} width="80" height="60" src="https://images.saasworthy.com/tr:w-160,h-0,c-at_max,e-sharpen-1/letsenhance_7500_logo_1627631413_kfyjo.png" alt="" />
+                      </div>
+                      <div className="product-details-info">
+                      <div className="product-name" >
+                            Let's Enhance
+                        </div>
+                          <div className="product-details">
+                          Let’s Enhance is a simple yet powerful image enhancement tool driven by Artificial Intelligence and machine learning to increase image resolution without losing the quality and colors.
+                      </div>
+                      </div>
+                        
+                   </div>
+                <div style={{paddingRight: "18px;"}} className="product-list-child-2">
+                    <div className="bar-2-details"> 
+                      <div style={{  color:"#818181", fontWeight:"500"}}>40 in stock</div>
+                      <div style={{ color:"black" , fontWeight:"bold"}}>$ 40.76</div>
+                      <div style={{  color:"#818181", fontWeight:"500"}}>10</div>
+                   </div>
+                </div>        
+            </div>
+            <div className="product-list-main">
+                <div className="product-list-child-1">
+                  <div>
+                      <img style={{borderRadius:"5px"}} width="80" height="50" src="https://images.saasworthy.com/tr:w-160,h-0,c-at_max,e-sharpen-1/adobephotoshoplightroom_31860_logo_1623674258_rhcn5.svg" alt="" />
+                      </div>
+                      <div className="product-details-info">
+                      <div className="product-name" > 
+                                Adobe Photoshop Lightroom
+                        </div>
+                          <div className="product-details">
+                          Easy-to-use tools like sliders and presets let you create photos that look just the way you want. 
+                      </div>
+                      </div>
+                        
+                   </div>
+                <div style={{paddingRight: "18px;"}} className="product-list-child-2">
+                    <div className="bar-2-details"> 
+                      <div style={{  color:"#818181", fontWeight:"500"}}>31 in stock</div>
+                      <div style={{ color:"black" , fontWeight:"bold" }}>$ 41.99</div>
+                      <div style={{  color:"#818181", fontWeight:"500"}}>75</div>
+                   </div>
+                </div>        
+            </div>
+            <div className="product-list-main">
+                <div className="product-list-child-1">
+                  <div> 
+                      <img style={{borderRadius:"5px"}} width="70" height="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Blender_logo_no_text.svg/2503px-Blender_logo_no_text.svg.png" alt="" />
+                      </div>
+                      <div className="product-details-info">
+                      <div className="product-name" > 
+                               Blender
+                        </div>
+                          <div className="product-details">
+                          Blender is a free and open source 3D creation suite available in under the GNU General Public License. 
+                      </div>
+                      </div>
+                        
+                   </div>
+                <div style={{paddingRight: "18px;"}} className="product-list-child-2">
+                    <div className="bar-2-details"> 
+                      <div style={{  color:"#818181", fontWeight:"500"}}>12 in stock</div>
+                      <div style={{ color:"black" , fontWeight:"bold" }}>$ 31.69</div>
+                      <div style={{  color:"#818181", fontWeight:"500"}}>29</div>
+                   </div>
+                </div>        
+            </div>
+            <div className="product-list-main">
+                <div className="product-list-child-1">
+                  <div> 
+                      <img style={{borderRadius:"5px"}} width="70" height="50" src="https://media.trustradius.com/product-logos/iB/LW/KTPEHA6R3IIU-180x180.JPEG" alt="" />
+                      </div>
+                      <div className="product-details-info">
+                      <div className="product-name" > 
+                               Google VR
+                        </div>
+                          <div className="product-details">
+                          Google VR is an SDK for building VR applications including two mobile VR platforms, Daydream and Cardboard. 
+                      </div>
+                      </div>
+                        
+                   </div>
+                <div style={{paddingRight: "18px;"}} className="product-list-child-2">
+                    <div className="bar-2-details"> 
+                      <div style={{ color:"#818181", fontWeight:"500"}}>89 in stock</div>
+                      <div style={{ color:"black" , fontWeight:"bold" }}>$ 11.69</div>
+                      <div style={{  color:"#818181", fontWeight:"500"}}>10</div>
+                   </div>
+                </div>        
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+          </div>
+               
+        
        
         
         </div>
---
+++ src/Components/asset/products.js
@@ -0,0 +1,28 @@
+const Product = [
+    {
+      id: 1,
+      name: "Adobe Photoshop Elements",
+      imgURL:
+        "https://images.saasworthy.com/tr:w-160,h-0,c-at_max,e-sharpen-1/adobephotoshopelements_5300_logo_1628071103_hschy.png",
+      phone: "+123 456 789",
+      email: "b@beyonce.com"
+    },
+    {
+      id: 2,
+      name: "Jack Bauer",
+      imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com"
+    },
+    {
+      id: 3,
+      name: "Chuck Norris",
+      imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com"
+    }
+  ];
+  
+  export default contacts;
